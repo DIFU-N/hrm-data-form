8ef5355277a30899c6a6e89d70a1b671e3b4d1a9
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-03 21:05:40 +0100

    get geoLocationList

---
+++ src/app/geolocation.js
@@ -0,0 +1,77 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+    loading: false,
+    locationList: [],
+    error: '',
+    selectedGeoLocation: {}
+}
+
+export const login = async () => {
+    const loginData = {
+      staffId: 15522,
+      password: "hussein",
+      adminPassword: "123",
+    };
+  
+    const response = await axios.post('https://genhive.onrender.com/auth/login', loginData, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+    return response.data.data.token;
+  }
+
+export const fetchGeoLocation = createAsyncThunk('locations/fetchGeoLocation', async () => {
+    const token = await login();
+    return await axios
+    .get('https://genhive.onrender.com/geoLocation', {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then((response) =>  {
+        console.log(response.data.data);
+        return response.data.data.map((user) => user.name)
+    })
+})
+
+
+export const geoLocaleSlice = createSlice({
+    name: 'geolocation',
+    initialState,
+    reducers: {
+        setSelectedGeoLocation: (state, action) => {
+           const {id, firstName, lastName, department, division, email, gender, location} = action.payload;
+           state.selectedGeoLocation = {}
+           state.selectedGeoLocation = {
+                ...state.selectedGeoLocation,
+                id, firstName, lastName, department, division, email, gender, location
+           }
+        //    console.log(state.selectedGeoLocation);
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchGeoLocation.pending, state => {
+            state.loading = true
+        })
+        builder.addCase(fetchGeoLocation.fulfilled, (state, action) => {
+            state.loading = false
+            state.locationList = [...action.payload]
+            console.log(state.locationList);
+            state.error = ''
+        })
+        builder.addCase(fetchGeoLocation.rejected, (state, action) => {
+            state.loading = false
+            state.locationList = []
+            console.log(action.error.message);
+            state.error = action.error.message
+        })
+    }
+});
+
+export default geoLocaleSlice.reducer;
+export const {setSelectedGeoLocation} = geoLocaleSlice.actions;
