83702dc624d254b3547f26ed16445cebe563e599
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-08 10:17:36 +0100

    filtering department from selected division..can't believe this is happening..aahhhh

--- src/components/FormComp.jsx
+++ src/components/FormComp.jsx
@@ -8,6 +8,7 @@ import {
   cug_validation,
   date_validation,
   department_validation,
+  division_validation,
   email_validation,
   employmentDateValidation,
   employment_status_validation,
@@ -43,69 +44,139 @@ const FormComp = () => {
     fontFamily: "Barlow, sans-serif",
   };
   const selectedStaff = useSelector((state) => state.staff.selectedStaff);
-  const selectedGeoLocation = useSelector((state) => state.geolocation.selectedGeoLocation);
-  const selectedDepartment = useSelector((state) => state.department.selectedDepartment);
+  const selectedGeoLocation = useSelector(
+    (state) => state.geolocation.selectedGeoLocation
+  );
+  const selectedDepartment = useSelector(
+    (state) => state.department.selectedDepartment
+  );
+  const selectedDivision = useSelector(
+    (state) => state.division.selectedDivision
+  );
   // console.log(selectedGeoLocation);
   // console.log(selectedStaff);
   const submitForm = ({ data }) => {
     // console.log(data);
-    const { id, firstName, lastName, middleName, 
-      email, gender, dob, phone1, phone2, address, nationality, employmentDate, state, category, workLocation,
+    const {
+      id,
+      firstName,
+      lastName,
+      middleName,
+      email,
+      gender,
+      dob,
+      phone1,
+      phone2,
+      address,
+      nationality,
+      employmentDate,
+      state,
+      category,
+      workLocationId,
     } = data || {};
     // console.log('this is' +phone1);
-    const cleanedNumber = (phone1).replace(/^0?\s*|\s*/g, "");
-    const phoneNumber = (phone1).startsWith("+234") ? phone1 : "+234" + cleanedNumber;
-    const editedNumber = (phoneNumber).replace(/^(\+234)|\s*/g, "");
-    editedNumber ? editedNumber : null
+    const cleanedNumber = phone1.replace(/^0?\s*|\s*/g, "");
+    const phoneNumber = phone1.startsWith("+234")
+      ? phone1
+      : "+234" + cleanedNumber;
+    const editedNumber = phoneNumber.replace(/^(\+234)|\s*/g, "");
+    editedNumber ? editedNumber : null;
     let textableNumber = null;
     if (editedNumber) {
-      return textableNumber = "0" + editedNumber;
+      return (textableNumber = "0" + editedNumber);
     }
     // if(phone1 || phone2) {
     //   return textableNumber;
     // }
+    //when you get the selectedGEoLocationId
+
     const parsedDob = dob ? new Date(dob) : null;
     const updatedData = {
       id: id || null,
       firstName: firstName || null,
-      lastName: lastName ||  null,
-      middleName: middleName ||  null,
-      email: email ||  null,
-      gender: gender ||  null,
+      lastName: lastName || null,
+      middleName: middleName || null,
+      email: email || null,
+      gender: gender || null,
       departmentId: selectedDepartment.id || null,
       geoLocationId: selectedGeoLocation.id || null,
-      // workLocation: workLocation ||  null,
-      dob: parsedDob ||  null,
-      phone1: textableNumber ||  null,
-      phone2: phone2 ||  null,
-      address: address ||  null,
-      // cugLine: cugLine ||  null,
-      nationality: nationality ||  null,
-      // employmentDate: employmentDate ||  null,
-      state: state ||  null,
-      category: category ||  null,
+      workLocationId: workLocationId || null,
+      dob: parsedDob || null,
+      phone1: textableNumber || null,
+      phone2: phone2 || null,
+      address: address || null,
+      CUGLine: CUGLine || null,
+      nationality: nationality || null,
+      employmentDate: employmentDate || null,
+      state: state || null,
+      category: category || null,
     };
-    console.log('Form data:', updatedData);
+    console.log("Form data:", updatedData);
     dispatch(updateStaff(updatedData));
   };
-  
+
   // const onSubmit = () => methods.handleSubmit(submitForm);
   const deptList = useSelector((state) => state.department.departmentList);
+  const divisionList = useSelector((state) => state.division.divisionList);
+  // console.log(divisionList);
   // console.log(selectedStaff.department);
-  const geoLocationList = useSelector((state) => state.geolocation.locationList);
+  const geoLocationList = useSelector(
+    (state) => state.geolocation.locationList
+    );
+  let selectedGeoLocationObject;
+  let selectedDepartmentObject
+  let selectedDivisionObject
+  
+  if (selectedStaff) {
+    selectedStaff.geoLocationId;
+    selectedGeoLocationObject = geoLocationList.find(
+      (location) => location.id === selectedStaff.geoLocationId 
+    );
+    // console.log(selectedGeoLocationObject);
+  }
+  if (selectedStaff) {
+    selectedStaff.departmentId;
+    selectedDepartmentObject = deptList.find(
+      (location) => location.id === selectedStaff.departmentId 
+    );
+    // console.log(selectedDepartmentObject);
+  }
+  if (selectedStaff) {
+    // selectedStaff.department.divisionId[0];
+    selectedDivisionObject = divisionList.find(
+      (division) => division.id === selectedStaff?.department?.divisionId[0] 
+    );
+    console.log(selectedDivisionObject);
+  }
   const transformedGeoLocationList = geoLocationList.map((location) => ({
     label: location.name,
-    value: location,
+    value: location.name,
   }));
-  const transformedDepartmentList = deptList.map((location) => ({
+  const transformedDivisionList = divisionList.map((location) => ({
     label: location.name,
-    value: location,
+    value: location.name,
   }));
+  let transformedDepartmentList;
+
+  if (selectedDivision && selectedDivision.id) {
+    transformedDepartmentList = deptList
+      .filter((department) => department.divisionId[0] === selectedDivision.id)
+      .map((department) => ({
+        label: department.name,
+        value: department.name,
+      }));
+  } else {
+    transformedDepartmentList = ['you have to choose a division first'];
+  }
+
+
+  // : ['you have to choose a division first'];
+
   return (
     <div className="w-[400px]">
       <FormProvider {...methods}>
         <form
-          onSubmit={methods.handleSubmit((data) => submitForm({data}))}
+          onSubmit={methods.handleSubmit((data) => submitForm({ data }))}
           noValidate
           autoComplete="off"
           className="container"
@@ -116,13 +187,19 @@ const FormComp = () => {
           >
             <div className="flex gap-3">
               <Input {...firstNameValidation} value={selectedStaff.firstName} />
-              <Input {...middleNameValidation} value={selectedStaff.middleName} />
+              <Input
+                {...middleNameValidation}
+                value={selectedStaff.middleName}
+              />
               <Input {...lastNameValidation} value={selectedStaff.lastName} />
             </div>
             <Input {...email_validation} value={selectedStaff.email} />
             <Input {...address_validation} value={selectedStaff.address} />
             <SelectDate {...date_validation} value={selectedStaff.dob} />
-            <Input {...nationality_validation} value={selectedStaff.nationality} />
+            <Input
+              {...nationality_validation}
+              value={selectedStaff.nationality}
+            />
             <Input {...state_validation} value={selectedStaff.state} />
             <PhoneInputField
               {...cell_validation}
@@ -137,17 +214,30 @@ const FormComp = () => {
               validation={validationSchema}
             /> */}
             <Select {...gender_validation} value={selectedStaff.gender} />
-            <SelectDate {...employmentDateValidation} value={selectedStaff.employmentDate} />
+            <SelectDate
+              {...employmentDateValidation}
+              value={selectedStaff.employmentDate}
+            />
             <Select {...category_validation} value={selectedStaff.category} />
-            <Select {...employment_status_validation} value={selectedStaff.employmentStatus}/>
+            <Select
+              {...employment_status_validation}
+              value={selectedStaff.employmentStatus}
+            />
             <Select
               {...geo_location_validation}
               options={transformedGeoLocationList}
-              value={selectedStaff.geoLocation}
+              value={selectedGeoLocationObject ? selectedGeoLocationObject.name : selectedStaff.geolocation }
             />
-
-
-            <Select {...department_validation} options={transformedDepartmentList} value={selectedStaff.department} />
+            <Select
+              {...division_validation}
+              options={transformedDivisionList}
+              value={selectedDivisionObject ? selectedDivisionObject.name : selectedStaff?.department?.division[0]?.name }
+            />
+            <Select
+              {...department_validation}
+              options={transformedDepartmentList}
+              value={selectedDepartmentObject ? selectedDepartmentObject.name : selectedStaff.department }
+            />
             <Input {...role_validation} value={selectedStaff.role} />
             {/* <PhoneInputField
               {...cug_validation}
